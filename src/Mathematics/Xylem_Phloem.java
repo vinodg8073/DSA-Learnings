73ab7a5ef6f88d9b7d3aa29d30171ed8c09de91d
Vinod G <vinodgowda1998@gmail.com>
2023-02-03 23:48:31 +0530

    Xylem Phloem

---
+++ src/Mathematics/Xylem_Phloem.java
@@ -0,0 +1,19 @@
+package Mathematics;
+
+public class Xylem_Phloem {
+	public static void main(String[] args) {
+		System.out.println(isXylemOrPhloem(1432));
+	}
+	
+	private static String isXylemOrPhloem(int n) {
+		int endNum = n%10;
+		n /=10;
+		int sum=0;
+		while (n>9) {
+			sum += n%10;
+			n /= 10;
+		}
+		
+		return ((endNum+n)==sum?"Xylem":"Phloem");
+	}
+}
