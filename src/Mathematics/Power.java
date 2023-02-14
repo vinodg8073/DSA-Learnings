5f747d93d2d27faebfaf0f3dd261d5a35471dcf8
Vinod G <vinodgowda1998@gmail.com>
2023-02-14 23:37:08 +0530

    Power

--- src/Mathematics/Power.java
+++ src/Mathematics/Power.java
@@ -3,10 +3,12 @@ package Mathematics;
 public class Power {
 
 	public static void main(String[] args) {
-		System.out.println(getPowerofN(3,4));
+		System.out.println(getPowerofN_1(3,4));
+		System.out.println(getPowerofN_2(3,4));
+		System.out.println(getPowerofN_3(3,4));
 	}
 	
-	 static public long getPowerofN(int base, int pow) {
+	 static public long getPowerofN_1(int base, int pow) {
 		 if (base==0) return 1;
 		 int temp=base;
 		 while(pow>1) {
@@ -16,4 +18,26 @@ public class Power {
 		 //Order TC-- theta of pow
 		return base;
 	}
+	 
+	 //TC and ASC both are O(log pow)
+	 static public long getPowerofN_2(int base, int pow) {
+		 if(pow==0) return 1;
+		 long temp= getPowerofN_2(base,pow/2);
+		 temp *=temp;
+		 if(pow%2==0) return temp;
+		 else return temp*base;
+	 }
+	 
+	 //TC is O(log pow) and ASC is O(1)
+	 static public long getPowerofN_3(int base, int pow) {
+		 if(base==0) return 1;
+		 int res =1;
+		 while(pow>0) {
+			 if (pow%2 != 0)
+				 res *= base;
+			 base *=base;
+			 pow/=2;
+		 }
+		 return res;
+	 }
 }
